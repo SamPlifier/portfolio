ed4919b9f089b5a486cc3220012cf70286cb6cf4
SamDC <sam.davis.castro@gmail.com>
2016-12-21 12:22:18 -0500

    Blank home page, contact info is complete, about info is complete, some dummy data is still in the portfolio section but layout is complete.

---
+++ js/controllers/MainController.js
@@ -0,0 +1,64 @@
+angular.module('portfolio', [])
+    .controller('MainController', ['$scope', function($scope, $timeout) {
+        $scope.menuClick = function(event) {
+            $scope.clearAll();
+            $scope.activate(event.currentTarget.id);
+        };
+        $scope.clearAll = function() {
+            $scope.showContact = false;
+            $scope.showAbout = false;
+            $scope.showPortfolio = false;
+            $('#contact').removeClass('active');
+            $('#about').removeClass('active');
+            $('#portfolio').removeClass('active');
+        };
+        $scope.activate = function(id) {
+            switch (id) {
+                case 'contact':
+                    $scope.showContact = true;
+                    $('#contact').addClass('active');
+                    break;
+                case 'about':
+                    $scope.showAbout = true;
+                    $('#about').addClass('active');
+                    break;
+                case 'portfolio':
+                    $scope.showPortfolio = true;
+                    $('#portfolio').addClass('active');
+                    break;
+                default:
+                    break;
+            }
+        };
+        $scope.projects = [{
+            icon: 'assets/asteroids.png',
+            name: 'Asteroids',
+            description: 'Made ship move.',
+            link: 'https://github.com/SamPlifier/Asteroids',
+        }, {
+            icon: 'assets/timeline.png',
+            name: 'Timeline',
+            description: 'CSS & media query practice',
+            link: 'https://github.com/SamPlifier/Timeline'
+        }, {
+            icon: 'assets/toDoList.png',
+            name: 'To-Do List',
+            description: 'jQuery, JS, Handlebars templates',
+            link: 'https://github.com/SamPlifier/to-do/settings'
+        }, {
+            icon: 'assets/tShirtShop.png',
+            name: 'T-Shirt Shop',
+            description: 'jQuery, JS, Handlebars templates',
+            link: 'https://github.com/SamPlifier/to-do/settings'
+        }, {
+            icon: 'assets/asteroids.png',
+            name: 'Asteroids',
+            description: 'Made ship move.',
+            link: 'https://github.com/SamPlifier/Asteroids',
+        }, {
+            icon: 'assets/timeline.png',
+            name: 'Timeline',
+            description: 'CSS & media query practice',
+            link: 'https://github.com/SamPlifier/Timeline'
+        }];
+    }]);
---
+++ js/directives/tiyProject.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//   var projectText = $('.projectText');
+//   projectText.mouseenter(function(){
+//     $('a').css({'color': 'white'});
+//   });
+//   projectText.mouseleave(function(){
+//     $('a').css({'color': '$transparent'});
+//   });
+// });
