0685758b879f8c034fd14a5c618ce2c81505d3e4
SamDC <sam.davis.castro@gmail.com>
2017-01-04 14:55:25 -0500

    In non-mobile views, Final Project nannydash is 600px wide (double the others.) Brightened and changed font when hovering on a project.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -50,49 +50,29 @@ angular.module('portfolio', [])
             navHeaders.toggle();
         });
         $scope.projects = [{
-            icon: 'assets/portfolioPics/iceCreamShip.png',
-            name: 'Ice Cream Shop',
-            description: 'Show some flavors in an html table',
-            link: '',
-        }, {
-            icon: 'assets/portfolioPics/blogLayout.png',
-            name: 'Blog Layout',
-            description: 'Use CSS to mimic TIY of Houston',
-            link: 'https://github.com/SamPlifier/Blog-Layout-hw'
-        }, {
-            icon: 'assets/portfolioPics/timeline.png',
-            name: 'Timeline',
-            description: 'Sass, CSS transitions & responsiveness',
-            link: 'https://github.com/SamPlifier/Timeline'
-        }, {
-            icon: 'assets/portfolioPics/tShirtShop.png',
-            name: 'T-Shirt Shop',
-            description: 'Sass, CSS transitions, responsiveness',
-            link: 'https://github.com/SamPlifier/to-do/settings'
-        }, {
-            icon: 'assets/portfolioPics/asteroids.png',
-            name: 'Asteroids',
-            description: 'Used JavaScript to move the ship',
-            link: 'https://github.com/SamPlifier/Asteroids',
-        }, {
-            icon: 'assets/portfolioPics/MovieRatings.png',
-            name: 'MovieLens',
-            description: 'Make AJAX requests to a movie database',
-            link: 'https://github.com/rdhelms/movieRatings'
-        }, {
-            icon: 'assets/portfolioPics/nprApiMobile.png',
-            name: 'NPR API',
-            description: 'Used open API, Handlebars & Grunt',
-            link: 'https://github.com/SamPlifier/NPRapi'
+            icon: 'assets/portfolioPics/nannydash.png',
+            name: 'Nannydash',
+            description: 'Final Project, multi-user dashboard',
+            link: 'https://github.com/lburl01/nannydash',
         }, {
             icon: 'assets/portfolioPics/planscape.png',
             name: 'Planscape',
             description: 'Front/Back-end project w/Angular & Ruby',
             link: 'https://github.com/donaldsonjulia/TIY-Planscape'
         }, {
-            icon: 'assets/portfolioPics/nannydash.png',
-            name: 'Nannydash',
-            description: 'Final Project, multi-user dashboard',
-            link: 'https://github.com/lburl01/nannydash',
+            icon: 'assets/portfolioPics/nprApiMobile.png',
+            name: 'NPR API',
+            description: 'Used open API, Handlebars & Grunt',
+            link: 'https://github.com/SamPlifier/NPRapi'
+        }, {
+            icon: 'assets/portfolioPics/MovieRatings.png',
+            name: 'MovieLens',
+            description: 'Make AJAX requests to a movie database',
+            link: 'https://github.com/rdhelms/movieRatings'
+        }, {
+            icon: 'assets/portfolioPics/asteroids.png',
+            name: 'Asteroids',
+            description: 'Used JavaScript to move the ship',
+            link: 'https://github.com/SamPlifier/Asteroids',
         }];
     }]);
