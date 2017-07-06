dedd6e360624069b2ff31edf63f7113333c96f35
SamDC <sam.davis.castro@gmail.com>
2017-07-05 23:59:40 -0400

    added new project and adjusted text for github or live links

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,6 +2,7 @@ angular.module('portfolio', [])
     .controller('MainController', ['$scope', function($scope) {
 
         var pageDefault = function() {
+            window.location.hash = 'contact';
             $scope.showContact = true;
             $('#contact').addClass('active');
         };
@@ -10,6 +11,8 @@ angular.module('portfolio', [])
         var navh2 = $('#contact, #about, #portfolio');
         pageDefault();
         $scope.menuClick = function(event) {
+            console.log(event.currentTarget.id);
+            window.location.hash = event.currentTarget.id;
             $scope.clearAll();
             $scope.activate(event.currentTarget.id);
             if ($(window).width() < 755) {
@@ -56,45 +59,60 @@ angular.module('portfolio', [])
         };
         $scope.projects = [
             {
-            icon: 'assets/portfolioPics/flightSightAviation.png',
-            name:'FlightSight Aviation',
-            description: 'Angular & UI-Bootstrap',
-            link: 'http://flightsightaviation.com/#/'
+                icon: 'assets/portfolioPics/goodThoughts.png',
+                name: 'Good Thoughts',
+                description: 'Non-profit site update',
+                link: 'http://www.goodthoughts.us',
+                siteLocation:'Live Site'
             },
-          {
-            icon: 'assets/portfolioPics/nannydash.png',
-            name: 'Nannydash',
-            description: 'Dashboard web app w/Angular and RoR',
-            link: 'https://github.com/lburl01/nannydash',
-          },
-          {
-            icon: 'assets/portfolioPics/planscape.png',
-            name: 'Planscape',
-            description: 'Angular, RoR, Google Maps API',
-            link: 'https://github.com/donaldsonjulia/TIY-Planscape'
-          },
-          {
-            icon: 'assets/portfolioPics/nprApiMobile.png',
-            name: 'NPR API',
-            description: 'Used open API, Handlebars & Grunt',
-            link: 'https://github.com/SamPlifier/NPRapi'
-          },
-          {
-            icon: 'assets/portfolioPics/toDoList.png',
-            name: 'To-Do List',
-            description: 'JavaScript and SASS',
-            link: 'https://samplifier.github.io/to-do/'
-          },
-          {
-            icon: 'assets/portfolioPics/chromesthesia.png',
-            name: 'Chromesthesia',
-            description: 'Paired notes & colors w/React',
-            link: 'https://samplifier.github.io/Chromesthesia/',
-          },
-          {
-          icon: 'assets/portfolioPics/calculator.png',
-          name: 'Calculator',
-          description: '4 function calculator w/JavaScript',
-          link: 'https://samplifier.github.io/fee-calculator/',
-        }];
+            {
+                icon: 'assets/portfolioPics/flightSightAviation.png',
+                name: 'FlightSight Aviation',
+                description: 'Angular & UI-Bootstrap',
+                link: 'http://flightsightaviation.com/#/',
+                siteLocation:'Live Site'
+            },
+            {
+                icon: 'assets/portfolioPics/nannydash.png',
+                name: 'Nannydash',
+                description: 'Dashboard web app w/Angular and RoR',
+                link: 'https://github.com/lburl01/nannydash',
+                siteLocation:'GitHub'
+            },
+            {
+                icon: 'assets/portfolioPics/planscape.png',
+                name: 'Planscape',
+                description: 'Angular, RoR, Google Maps API',
+                link: 'https://github.com/donaldsonjulia/TIY-Planscape',
+                siteLocation:'GitHub'
+            },
+            {
+                icon: 'assets/portfolioPics/nprApiMobile.png',
+                name: 'NPR API',
+                description: 'Used open API, Handlebars & Grunt',
+                link: 'https://github.com/SamPlifier/NPRapi',
+                siteLocation:'GitHub'
+            },
+            {
+                icon: 'assets/portfolioPics/toDoList.png',
+                name: 'To-Do List',
+                description: 'JavaScript and SASS',
+                link: 'https://samplifier.github.io/to-do/',
+                siteLocation:'Live Example'
+            },
+            {
+                icon: 'assets/portfolioPics/chromesthesia.png',
+                name: 'Chromesthesia',
+                description: 'Paired notes & colors w/React',
+                link: 'https://samplifier.github.io/Chromesthesia/',
+                siteLocation:'Live Site'
+            },
+            {
+                icon: 'assets/portfolioPics/calculator.png',
+                name: 'Calculator',
+                description: '4 function calculator w/JavaScript',
+                link: 'https://samplifier.github.io/fee-calculator/',
+                siteLocation:'Live Site'
+            }
+        ];
     }]);
